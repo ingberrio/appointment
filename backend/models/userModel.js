75708dd0d2c9592dec386af12dab3ebcb62988a3
Eduard Berrio <genial@duck.com>
2023-11-13 08:17:16 -0500

    build: backend added

---
+++ backend/models/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = mongoose.Schema(
+    {
+      userName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
+      },
+      password: {
+        type: String,
+        required: true,
+        minlength: 6,
+      },
+      role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+      }
+    },
+    {
+      timestamps: true,
+    }
+);
+
+// Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
